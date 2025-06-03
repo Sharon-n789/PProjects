371cba8c8ad0df0127b6a0c0d4947a20859dc1ff
Sharon-n789 <sharon060504@gmail.com>
2025-06-03 09:58:16 +0530

    Adding in the folder format

--- TurtleCrossing/main.py
+++ TurtleCrossing/main.py
@@ -1,6 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from player import Player
-from car_manager import CarManager
+from TurtleCrossing.car_manager import CarManager
 from scoreboard import Scoreboard
 import time
 
