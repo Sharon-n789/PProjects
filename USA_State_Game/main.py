77b56cb7ceb60887a882a9ba97056b9a066ac25e
Sharon-n789 <sharon060504@gmail.com>
2025-08-25 23:40:23 +0530

    Adding a new state game

---
+++ USA_State_Game/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+import pandas
+turtle = Turtle()
+t = Turtle()
+screen = Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+# def get_mouse_click_coor(x, y):
+#   print(x, y)
+
+
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        state_data = data[data.state == answer_state]
+        t.hideturtle()
+        t.penup()
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(state_data.state.item())
+
+
+
